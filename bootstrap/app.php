f4c3da79e2a869c49e7281593138c396aba7ba41
danjay718-ai <dannverjay.lagramada718@gmail.com>
2026-06-19 19:20:18 +0800

    feat(middleware): append UpdateUserOnlineStatus middleware to web group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->appendToGroup('web', \App\Http\Middleware\UpdateUserOnlineStatus::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
